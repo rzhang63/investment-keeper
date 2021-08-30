5e7906c5d0645741595d14e3f3ce9182bfeac207
Ruotao <52050866+rzhang63@users.noreply.github.com>
2021-08-30 14:39:09 -0400

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -4,6 +4,13 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]

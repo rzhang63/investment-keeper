dc7fef6ff49e54a1900ef5393f394e655d2735f1
Ruotao <52050866+rzhang63@users.noreply.github.com>
2021-08-30 15:19:45 -0400

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -7,7 +7,7 @@ import snowflake.connector
 # Uses st.cache to only run once.
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return snowflake.connector.connect(**st.secrets["s1"])
+    return snowflake.connector.connect(**st.secrets["gcp_service_account"])
 
 conn = init_connection()
 

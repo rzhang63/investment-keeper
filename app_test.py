ff6523c40947e901cf62acd78d2e7d54f8428d62
Ruotao <52050866+rzhang63@users.noreply.github.com>
2021-08-30 15:30:09 -0400

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -7,18 +7,20 @@ import snowflake.connector
 # Uses st.cache to only run once.
 #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return snowflake.connector.connect(**st.secrets["s1"])
+    return snowflake.connector.connect(**st.secrets["snowflake"])
 
 conn = init_connection()
 
-
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
+#@st.cache(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
 rows = run_query("SELECT * from mytable;")
-st.write(rows)
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")

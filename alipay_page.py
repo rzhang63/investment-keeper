47ef1d1b6e33092bb637476ccf66bbb4ff3a1900
Ruotao <52050866+rzhang63@users.noreply.github.com>
2021-08-31 09:47:53 -0400

    Update alipay_page.py

--- alipay_page.py
+++ alipay_page.py
@@ -89,7 +89,7 @@ def load_and_save_transaction_file(file):
 
 def load_and_save_asset_file(file):
     # read in asset values
-    assets_df = tabula.read_pdf(file,pages='all')#[1].iloc[1: , :][['基金代码','资产小计','单位净值日期']]
+    assets_df = tabula.read_pdf(file,pages='all')[1].iloc[1: , :][['基金代码','资产小计','单位净值日期']]
     st.write(assets_df)
     #assets_df = pd.read_excel(file, skiprows=[0, 1, 2])[['基金代码','资产小计','单位净值日期']]
     assets_df.columns = ['CODE','ASSET','DATE']

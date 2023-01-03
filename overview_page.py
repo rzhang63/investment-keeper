2dbdae61b8c6ffb57e4fcc68340d1461a4783b14
Ruotao <52050866+ruotaozhang@users.noreply.github.com>
2023-01-03 10:13:53 -0500

    Update overview_page.py

--- overview_page.py
+++ overview_page.py
@@ -142,7 +142,7 @@ def main():
 
     total_value_df = utils.load_worksheet('totalValue',sh).sort_values(by=['date'],ascending=False)#.astype({'date': 'datetime'})
     total_value_df = total_value_df.where(total_value_df["user"]==st.session_state['user'])
-    st.write(total_value_df['amount'])
+    st.write(total_value_df['amount'].item())
 
 
     st.subheader("CHAU")
